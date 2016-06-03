1da3a8086edb3d89a6e5e612f731adcf933fb0d6
timstone <tim.stone@yahoo.co.uk>
2016-06-03 15:36:12 +0100

    wip

---
+++ features/limited_company_registration.feature
@@ -0,0 +1,14 @@
+Feature: Limited company registers flood risk activity exemption
+  As a limited company
+  I want to register a location for a flood risk activity exemptions
+  So that I can check that my activity does not harm the environment in that area
+
+  Background: 
+  Given I register for a new exemption
+
+  @happy_path
+  Scenario: Limited company registers location for flood risk activity exemption
+  Given I register exemption FRA1
+  And I register as a limited company
+  When I confirm my registration
+  Then I will be informed that my application has been received
---
+++ features/step_definitions/limited_company_registration_steps.rb
@@ -0,0 +1,63 @@
+Given(/^I register for a new exemption$/) do
+    @app = App.new
+  	@app.check_location_page.load
+end
+
+Given(/^I register exemption FRA(\d+)$/) do |arg1|
+  
+  @app.add_exemption_page.wait_for_check_boxes
+
+  @exemption_number == arg1
+  
+  @app.add_exemption_page.check_boxes.find { |chk| chk.value == @arg1 }.click
+
+  @app.add_exemption_page.submit_button.click
+  
+  @app.check_exemptions_page.wait_for_submit_button
+
+  @app.check_exemptions_page.submit_button.click
+
+  @app.grid_reference_page.wait_for_submit_button
+  @app.grid_reference_page.fill_grid_ref.set "ST 58132 72695"
+  @app.grid_reference_page.fill_site_description.set "Location of activity"
+  @app.grid_reference_page.submit_button.click
+  
+  # Grid reference page
+  @app.grid_reference_page.wait_for_submit_button
+  @app.grid_reference_page.fill_grid_ref.set "ST 58132 72695"
+  @app.grid_reference_page.fill_site_description.set "Location of activity"
+  # fill_in('grid_reference_grid_reference', :with => 'ST 58132 72695')
+  @app.grid_reference_page.submit_button.click
+  # click_button 'Continue'
+  
+  
+  # User type page
+  @app.user_type_page.wait_for_submit_button
+  @app.user_type_page.radio_buttons.find { |btn| btn.value == 'limited_company' }.click
+
+  @app.user_type_page.submit_button.click
+
+  #Organisation name page
+  # @app.organisation_name_page.wait_for_submit_button
+  
+  # @app.organisation_name_page.fill_local_authority_name.set "Testminster council"
+  
+  # @app.organisation_name_page.submit_button.click
+
+  #Address page - post code lookup
+  # @app.postcode_page.wait_for_submit_button
+  # @app.postcode_page.enter_postcode.set "BS1 5AH"
+  # @app.postcode_page.submit_button.click
+
+  #Address page - select address from post code lookup list
+  # @app.address_page.wait_for_submit_button
+  # @app.address_page.wait_for_show_list
+ 
+  # @app.address_page.results.find(:xpath, 'option[2]').select_option
+  # find('#address_match_selection').find(:xpath, 'option[2]').select_option
+  # @app.address_page.submit_button.click
+  end
+
+Given(/^I register as a limited company$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
