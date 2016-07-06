360bf9c3e73c824ab32e7c595d1d785680cc211f
timstone <tim.stone@yahoo.co.uk>
2016-07-06 08:36:13 +0100

    replaced email addresses

--- features/support/page_objects/check_location_page.rb
+++ features/support/page_objects/check_location_page.rb
@@ -1,6 +1,6 @@
 # Check location page
 class CheckLocationPage < SitePrism::Page
-  set_url 'https://floodrisk-fo-qa.herokuapp.com/fre/enrollments/new'
+  set_url 'https://frae-prod.envage.co.uk/fre/enrollments/new'
 
   elements :radio_buttons, "input[name='check_location[location_check]']"
   element  :submit_button, "input[name='commit']"
