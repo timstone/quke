b95aefccfe41109976d52d649f2c164e050b9c4a
timstone <tim.stone@yahoo.co.uk>
2016-07-12 13:30:33 +0100

    changing url to QA env

--- features/support/page_objects/Front_office/check_location_page.rb
+++ features/support/page_objects/Front_office/check_location_page.rb
@@ -1,7 +1,7 @@
 # Check location page
 class CheckLocationPage < SitePrism::Page
-  	set_url 'https://floodrisk-fo-qa.herokuapp.com/fre/enrollments/new'
-	# set_url 'https://frae-qa.envage.co.uk/fre/enrollments/new'
+  	# set_url 'https://floodrisk-fo-qa.herokuapp.com/fre/enrollments/new'
+	set_url 'https://frae-qa.envage.co.uk/fre/enrollments/new'
   	elements :radio_buttons, "input[name='check_location[location_check]']"
   	element  :submit_button, "input[name='commit']"
 end
